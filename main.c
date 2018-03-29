60b5cddd744e521ac6075bc7e73b2fe4d803e95e
Aashman <aashmangupta@gmail.com>
2018-03-28 23:44:01 -0500

    Done for today

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include<stdlib.h>
 #include<ctype.h>
 
+char dataTable[26][2];
+
+void populateTable(char currChar);
+
 int main(int argc, char** argv) {
-	if(argc < 1) {
+	int count = 0;
+	char currChar;
+
+	if(argc < 2) {
 		printf("Type in a data file after ./a.out.\n");
 		return 0;
 	}
@@ -11,22 +18,32 @@ int main(int argc, char** argv) {
 	char* dataFile = argv[argc - 1];
 	
 	FILE* fp = fopen(dataFile, "r");
-	char currChar[30][1];				//make a loop so that characters get put in the place they belong in the alphabet.
-	int count = 0;
-
-	fscanf(fp, "%c", &currChar[0][0]);
+										//Make a loop so that characters get put in the place they belong in the alphabet.
+	for(int i = 0; i < 26; i++) {		//This loop puts in initial value into 2D array.
+		currChar = i;					//FIXME... Can't I just use a single array?
+		printf("%c", currChar);			//FIXME problem with converting to ASCII and back
+		dataTable[i][0] = currChar;		//...Refer to index through ASCII values...
+		dataTable[i][1] = 0;
+	}
 	
+	fscanf(fp, "%c", &currChar);
 	while(!feof(fp)) {
-		currChar = tolower(currChar[count][0]);
-		
-		if() {
-
-		}
-
-		printf("%c", currChar);
+		populateTable(currChar);
 		fscanf(fp, "%c", &currChar);	
-		//find the character and incerement its frequency in the array
 	}
 
+	for(int i = 0; i < 26; i++) {
+		printf("%c", dataTable[i][0]);
+		printf(": %c", dataTable[i][1]);
+	}
+	printf("\n");
+
 return 0;
+}
+
+void populateTable(char currChar) {
+	//int index = currChar - 97;
+	//int oldFreq = dataTable[index][1];
+
+	//dataTable[index][1] = oldFreq++;
 }
